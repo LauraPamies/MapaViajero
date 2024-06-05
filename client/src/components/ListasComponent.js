845aecba972706be7526540ccb48f36d0664785f
LauraPamies <laurapamiesvillagordo@gmail.com>
2024-06-05 21:55:05 +0200

    Filtros completados

--- client/src/components/ListasComponent.js
+++ client/src/components/ListasComponent.js
@@ -19,19 +19,22 @@ const ListasComponent = () => {
 
     };
 
+    const opciones_ordenar = [
+        { label: "Mas barato primero", value: "precio_asc" },
+        { label: "Mas caro primero", value: "precio_desc" },
+        { label: "Mas nuevos primero", value: "fecha_desc" },
+        { label: "Mas antiguos primero", value: "fecha_asc" }
+    ]
 
+    const [HaBuscado, setHaBuscado] = useState(false);
 
-    // const todasEtiquetas = ['Todo',...new Set(itinerarios.map(itinerario => itinerario.etiqueta))]
-    // console.log(todasEtiquetas);
-
-    const [orden, setOrden] = useState('');
-
+    const [datosBusqueda, setDatosBusqueda] = useState([]);
     const [itinerarios, setItinerarios] = useState([]);
     const [itinerariosPrincipio, SetitinerariosPrincipio] = useState([]);
-    
+
     const [Etiquetas, setEtiquetas] = useState(['Todo']);
 
-   
+
 
     const navigate = useNavigate();
     //FORMULARIO
@@ -46,13 +49,6 @@ const ListasComponent = () => {
 
         if (localStorage.getItem('isLoggedIn') !== 'true') { //SI NO ESTÁ LOGUEADO
             navigate("/login");
-        } else {
-            //SI ESTÁ LOGUEADO
-            console.log(localStorage.getItem('userId'));
-            console.log(localStorage.getItem('userName'));
-            console.log(localStorage.getItem('userEmail'));
-            console.log(localStorage.getItem('isLoggedIn'));
-
         }
 
         async function fetchData() {
@@ -83,11 +79,47 @@ const ListasComponent = () => {
         navigate(`/itinerario/${id}`);
     };
 
-    const filtrarEtiquetas = (etiqueta) =>{
-        if(etiqueta === 'Todo') {
+    const orden_cambiado = (async (event) => {
+
+        //Cada vez que el orden cambie hace otra petición
+        if (HaBuscado === true) {
+            try {
+                const response = await axios.post('http://localhost:3050/ItinerariosConcretos', {
+                    destino: datosBusqueda.destino,
+                    dias: datosBusqueda.dias,
+                    personas: datosBusqueda.personas,
+                    pre_min: datosBusqueda.pre_min,
+                    pre_max: datosBusqueda.pre_max,
+                    orden: event.target.value
+                });
+                setItinerarios(response.data);
+                SetitinerariosPrincipio(response.data);//Duplica el array para que haya uno que los tenga todos siempre(el itinerariosPrincipio) y otro con los filtrados
+
+            } catch (error) {
+                console.error('Error al obtener los itinerarios:', error);
+            }
+        }
+        else {
+            try {
+                const response = await axios.post('http://localhost:3050/getItinerarios', {
+                    orden: event.target.value
+                });
+                setItinerarios(response.data);
+                SetitinerariosPrincipio(response.data);//Duplica el array para que haya uno que los tenga todos siempre(el itinerariosPrincipio) y otro con los filtrados
+
+
+            } catch (error) {
+                console.error('Error al obtener los itinerarios:', error);
+            }
+        }
+
+    });
+
+    const filtrarEtiquetas = (etiqueta) => {
+        if (etiqueta === 'Todo') {
             const itinerariosFiltrados = itinerariosPrincipio.filter(itinerario => itinerario.etiqueta.trim() !== '');
             setItinerarios(itinerariosFiltrados);
-            return 
+            return
         }
 
         const itinerariosFiltrados = itinerariosPrincipio.filter(itinerario => itinerario.etiqueta === etiqueta);
@@ -96,11 +128,17 @@ const ListasComponent = () => {
     }
 
     const onSubmit = handleSubmit(async (data) => {
-        console.log(data);
-        var orden = "";
-
+        setDatosBusqueda(data);
+        setHaBuscado(true);
         try {
-            const response = await axios.post('http://localhost:3050/ItinerariosConcretos', data);
+            const response = await axios.post('http://localhost:3050/ItinerariosConcretos', {
+                destino: data.destino,
+                dias: data.dias,
+                personas: data.personas,
+                pre_min: data.pre_min,
+                pre_max: data.pre_max,
+                orden: ""
+            });
             setItinerarios(response.data);
             SetitinerariosPrincipio(response.data);//Duplica el array para que haya uno que los tenga todos siempre(el itinerariosPrincipio) y otro con los filtrados
 
@@ -110,7 +148,7 @@ const ListasComponent = () => {
 
     });
 
-    
+
 
     return (
         <div className='Listasdiv'>
@@ -232,56 +270,69 @@ const ListasComponent = () => {
 
                 <button id='botonmapa' onClick={ir_mapa}>Ver mapa</button>
 
+                <div className='filtros_y_itinerarios'>
 
-
-                <div className='etiquetas'>
-                    {Etiquetas.map(etiqueta=>(
-                        <label key={etiqueta}>
-                        <input 
-                        type="radio" 
-                        value={etiqueta} 
-                        name="etiqueta"
-                        onClick={()=> filtrarEtiquetas(etiqueta)}
-                        />
-                            {etiqueta}
-                            </label>
-                    ))}
-                </div>
-
-
-                <div className="itinerarios-container">
-                    {itinerarios.map((itinerario, index) => (
-                        <div key={itinerario.id} className="itinerario-card" onClick={() => handleItinerarioClick(itinerario.id)}
-                            style={{ cursor: 'pointer' }}>
-                            {/* <img src={itinerario.foto} alt={itinerario.titulo} className="itinerario-imagen" /> */}
-                            <div className="image-container">
-                                <img
-                                    src={`${process.env.PUBLIC_URL}/fotos_itinerarios/${itinerario.foto.split('/').pop()}`}
-                                    alt={itinerario.titulo}
-                                    className="itinerario-imagen" />
-                            </div>
-                            <div className="itinerario-info">
-                                <div>
-                                    <h2 className="itinerario-titulo">{itinerario.titulo}</h2>
-                                </div>
-
-                                <div id='iti-dias-precio'>
-                                    <p>{itinerario.dias} días</p>
-                                    <p className="itinerario-precio">{itinerario.precio}€</p>
-
-                                </div>
-                                <div id='iti-personas'>
-                                    <p>{itinerario.personas} personas</p>
-                                    <p className="itinerario-autor">{itinerario.name}</p>
-
-
-                                </div>
-
-
-                            </div>
-
+                    <div className='filtros_container'>
+                        <h4>Ordenar por</h4>
+                        <select className='orden_select' onChange={orden_cambiado}>
+                            {opciones_ordenar.map(opcion => (
+                                <option value={opcion.value}>{opcion.label}</option>
+                            ))}
+                        </select>
+                        <div className='etiquetas'>
+                        <h4>Tipo de viaje</h4>
+                        <div >
+                            {Etiquetas.map(etiqueta => (
+                                <label key={etiqueta} style={{ display: "block" }}>
+                                    <input
+                                        type="radio"
+                                        value={etiqueta}
+                                        name="etiqueta"
+                                        onClick={() => filtrarEtiquetas(etiqueta)}
+                                    />
+                                    {etiqueta}
+                                </label>
+                            ))}
                         </div>
-                    ))}
+                        </div>
+                        
+                        
+                    </div>
+
+                    <div className="itinerarios-container">
+                        {itinerarios.map((itinerario, index) => (
+                            <div key={itinerario.id} className="itinerario-card" onClick={() => handleItinerarioClick(itinerario.id)}
+                                style={{ cursor: 'pointer' }}>
+                                {/* <img src={itinerario.foto} alt={itinerario.titulo} className="itinerario-imagen" /> */}
+                                <div className="image-container">
+                                    <img
+                                        src={`${process.env.PUBLIC_URL}/fotos_itinerarios/${itinerario.foto.split('/').pop()}`}
+                                        alt={itinerario.titulo}
+                                        className="itinerario-imagen" />
+                                </div>
+                                <div className="itinerario-info">
+                                    <div>
+                                        <h2 className="itinerario-titulo">{itinerario.titulo}</h2>
+                                    </div>
+
+                                    <div id='iti-dias-precio'>
+                                        <p>{itinerario.dias} días</p>
+                                        <p className="itinerario-precio">{itinerario.precio}€</p>
+
+                                    </div>
+                                    <div id='iti-personas'>
+                                        <p>{itinerario.personas} personas</p>
+                                        <p className="itinerario-autor">{itinerario.name}</p>
+
+
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
 
--- server/src/controllers/itinerarios.controller.js
+++ server/src/controllers/itinerarios.controller.js
@@ -18,7 +18,7 @@ export const getItinerarios = async (req, res) => { //req = request, osea los va
         }
 
         //SI RECIBE PRECIO DESCENDENTE
-        else if (orden == "precio_des") {
+        else if (orden == "precio_desc") {
             console.log("select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id  order by precio DESC;");
             const [result] = await pool.query("select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id  order by precio DESC;")
 
@@ -34,7 +34,7 @@ export const getItinerarios = async (req, res) => { //req = request, osea los va
         }
 
         //SI RECIBE FECHA DESCENDENTE
-        else if (orden == "fecha_des") {
+        else if (orden == "fecha_desc") {
             console.log("select iti.id,titulo,fecha,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id  order by fecha DESC;");
             const [result] = await pool.query("select iti.id,titulo,fecha,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id  order by fecha DESC;")
 
@@ -87,13 +87,49 @@ export const ItinerariosConcretos = async (req, res) => { //req = request, osea 
         // throw new Error('sample') //MANDAR ERRORES DE PRUEBA
 
         console.log(req.body);
-        const { destino, dias, personas, pre_min, pre_max } = req.body;
+        const { destino, dias, personas, pre_min, pre_max, orden } = req.body;
         //SI RECIBE PRECIO ASCENDENTE
-        console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "';");
-        const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? ;", [destino, dias, personas, pre_min, pre_max])
+        if (orden == "precio_asc") {
+            console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "' order by precio ASC;");
+            const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? order by precio ASC;", [destino, dias, personas, pre_min, pre_max])
+    
+    
+            res.send(result);
+        }
+        
+        //SI RECIBE PRECIO DESCENDENTE
+        else if (orden == "precio_desc") {
+            console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "' order by precio DESC;");
+            const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? order by precio DESC;", [destino, dias, personas, pre_min, pre_max])
+    
+    
+            res.send(result);
+        }
 
+         //SI RECIBE FECHA ASCENDENTE
+         else if (orden == "fecha_asc") {
+            console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "' order by fecha ASC;");
+            const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? order by fecha ASC;", [destino, dias, personas, pre_min, pre_max])
+    
+    
+            res.send(result);
+        }
 
-        res.send(result);
+        //SI RECIBE FECHA DESCENDENTE
+        else if (orden == "fecha_desc") {
+            console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "' order by fecha DESC;");
+            const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? order by fecha DESC;", [destino, dias, personas, pre_min, pre_max])
+    
+    
+            res.send(result);
+        }
+        else{
+            console.log("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = '" + destino + "' AND dias = '" + dias + "' AND personas = '" + personas + "' AND precio between '" + pre_min + "' AND '" + pre_max + "' ;");
+            const [result] = await pool.query("Select iti.id,titulo,foto,etiqueta,dias,personas,precio,name from itinerarios AS iti INNER JOIN usuarios AS us ON iti.autor_id = us.id WHERE titulo = ? AND dias = ? AND personas = ? AND precio between ? AND ? ;", [destino, dias, personas, pre_min, pre_max])
+    
+    
+            res.send(result);
+        }
 
 
     } catch (error) {
